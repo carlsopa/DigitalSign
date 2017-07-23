25540c6b42dc81e2b278b990d366f255b0f0a4f6
Michelle Minkoff <mminkoff@ap.org>
2017-07-23 19:24:48 -0400

    Addresses small bugs with preventing default on submit button, accessing bus stop id, and writing out a stray key name when we were generating URL params dynamically via jQuery

--- TestScript/testc.js
+++ TestScript/testc.js
@@ -1,6 +1,7 @@
 $(function(){
-  $(".button").click(function(){
+  $(".button").click(function(event){
     LatLonCall();
+    event.preventDefault();
   })
 
 });
@@ -14,16 +15,16 @@ function LatLonCall(){
   alert("https://maps.googleapis.com/maps/api/geocode/json?" + "address="+FullAddress+"&key=AIzaSyAGt3oFrXSziqC7au4sGmMTadhBbqfjstY");
 
   var params = {"address":FullAddress,"key":"AIzaSyAGt3oFrXSziqC7au4sGmMTadhBbqfjstY"}
-  //$.ajax({url: "https://maps.googleapis.com/maps/api/geocode/json?" + "address="+FullAddress+"&key=AIzaSyAGt3oFrXSziqC7au4sGmMTadhBbqfjstY",type: "GET",})
-  $.ajax({url: "https://api.wmata.com/Bus.svc/json/jStopSchedule?StopID=3002578&api_key=f9a8294236b6475990ef9d0085bc3826",type: "POST",})
+  $.ajax({url: "https://maps.googleapis.com/maps/api/geocode/json?" + "address="+FullAddress+"&key=AIzaSyAGt3oFrXSziqC7au4sGmMTadhBbqfjstY",type: "GET",})
+//  $.ajax({url: "https://api.wmata.com/Bus.svc/json/jStopSchedule?StopID=3002578&api_key=f9a8294236b6475990ef9d0085bc3826",type: "POST",})
 
   .done(function(data){
     console.log("FINALLY");
-    //lat = data.results[0].geometry.location.lat;
-    //lon = data.results[0].geometry.location.lng;
-    //console.log(lat);
-    //console.log(lon);
-    //StationCall(lat,lon);
+    lat = data.results[0].geometry.location.lat;
+    lon = data.results[0].geometry.location.lng;
+    console.log(lat);
+    console.log(lon);
+    StationCall(lat,lon);
   })
   .fail(function(data){
     console.log(data);
@@ -38,13 +39,13 @@ function StationCall(lat,lon){
   var params = {
             "api_key": "f9a8294236b6475990ef9d0085bc3826",
             "Lat": Latitude,"Lon": Longitude,"Radius": "300",};
-  //alert(api_key);
-  //alert(Latitude);
-  //alert(Longitude);
+  alert(api_key);
+  alert(Latitude);
+  alert(Longitude);
   console.log("https://api.wmata.com/Bus.svc/json/jStops?Lat="+Latitude+"&Lon="+Longitude+"&Radius="+Radius+"&api_key="+api_key);
   //$.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?Lat="+Latitude+"&Lon="+Longitude+"&Radius="+Radius+"&api_key="+api_key,type: "GET",})
-  //$.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?Lat="+$.param(params),type: "GET",})
-  $.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?Lat=38.9200763&Lon=-77.0387962&Radius=300&api_key=f9a8294236b6475990ef9d0085bc3826",type: "GET"})
+  $.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?"+$.param(params),type: "GET"})
+  //$.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?Lat=38.9200763&Lon=-77.0387962&Radius=300&api_key=f9a8294236b6475990ef9d0085bc3826",type: "GET"})
   .done(function(result){
     console.log("PAUL IS AWESOME!");
     alert("FINALLY WORKING!!");
@@ -62,10 +63,10 @@ function StationCall(lat,lon){
 }
 function BusSchedule(response){
 var api_key = "f9a8294236b6475990ef9d0085bc3826";
-var StopID = response.Stops[0].StopID;
+var StopID = response;
 $.ajax({url: "https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID="+StopID+"&api_key="+api_key,type: "GET",})
 .done(function(data){
-
+  console.log(data)
 })
 .fail(function(data){
   alert("BusSchedule Failure!");
