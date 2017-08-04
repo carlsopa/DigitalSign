61a7346f6be9961b56a51f93871fde2c56c94e41
Michelle Minkoff <mminkoff@ap.org>
2017-08-03 23:02:22 -0400

    Re-enables form, tinkers with CSS, defaults in form which clear on click

--- TestScript/testc.js
+++ TestScript/testc.js
@@ -1,25 +1,21 @@
-<<<<<<< HEAD
 $(document).ready(function(){
-  //$(".button").click(function(event){
-    LatLonCall();
-    //event.preventDefault();
-  //})
-=======
-$(function(){
   $(".button").click(function(event){
     LatLonCall();
     event.preventDefault();
   })
->>>>>>> origin/master
+
+  $('form input:text').click(function(e){
+    e.target.value=""
+  })
 
 });
 function LatLonCall(){
-  //var address = $("input#address").val();
-  var address = "1801 Belmont St NW";
-  //var city = $("input#city").val();
-  var city = "Washington";
-  //var state = $("input#state").val();
-  var state = "DC";
+  var address = $("input#address").val();
+  //var address = "1801 Belmont St NW";
+  var city = $("input#city").val();
+  //var city = "Washington";
+  var state = $("input#state").val();
+ // var state = "DC";
   var DataString = 'address'+address+'city'+city+'state'+state;
   var FullAddress = address+", "+city+", "+state;
   FullAddress = FullAddress.replace(/ /g,"+");
@@ -27,11 +23,9 @@ function LatLonCall(){
 
   var params = {"address":FullAddress,"key":"AIzaSyAGt3oFrXSziqC7au4sGmMTadhBbqfjstY"}
   $.ajax({url: "https://maps.googleapis.com/maps/api/geocode/json?" + "address="+FullAddress+"&key=AIzaSyAGt3oFrXSziqC7au4sGmMTadhBbqfjstY",type: "GET",})
-<<<<<<< HEAD
-=======
-//  $.ajax({url: "https://api.wmata.com/Bus.svc/json/jStopSchedule?StopID=3002578&api_key=f9a8294236b6475990ef9d0085bc3826",type: "POST",})
 
->>>>>>> origin/master
+  //$.ajax({url: "https://api.wmata.com/Bus.svc/json/jStopSchedule?StopID=3002578&api_key=f9a8294236b6475990ef9d0085bc3826",type: "POST",})
+
   .done(function(data){
     console.log("FINALLY");
     lat = data.results[0].geometry.location.lat;
@@ -39,10 +33,7 @@ function LatLonCall(){
     console.log(lat);
     console.log(lon);
     StationCall(lat,lon);
-<<<<<<< HEAD
     WeatherCall(lat,lon);
-=======
->>>>>>> origin/master
   })
   .fail(function(data){
     console.log(data);
@@ -57,10 +48,8 @@ function StationCall(lat,lon){
   var params = {
             "api_key": "f9a8294236b6475990ef9d0085bc3826",
             "Lat": Latitude,"Lon": Longitude,"Radius": "300",};
-<<<<<<< HEAD
   console.log("https://api.wmata.com/Bus.svc/json/jStops?Lat="+Latitude+"&Lon="+Longitude+"&Radius="+Radius+"&api_key="+api_key);
   $.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?"+$.param(params),type: "GET"})
-=======
   alert(api_key);
   alert(Latitude);
   alert(Longitude);
@@ -68,7 +57,6 @@ function StationCall(lat,lon){
   //$.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?Lat="+Latitude+"&Lon="+Longitude+"&Radius="+Radius+"&api_key="+api_key,type: "GET",})
   $.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?"+$.param(params),type: "GET"})
   //$.ajax({url: "https://api.wmata.com/Bus.svc/json/jStops?Lat=38.9200763&Lon=-77.0387962&Radius=300&api_key=f9a8294236b6475990ef9d0085bc3826",type: "GET"})
->>>>>>> origin/master
   .done(function(result){
     console.log("PAUL IS AWESOME!");
     var id= result.Stops[0].StopID;
@@ -88,18 +76,14 @@ var api_key = "f9a8294236b6475990ef9d0085bc3826";
 var StopID = response;
 $.ajax({url: "https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID="+StopID+"&api_key="+api_key,type: "GET",})
 .done(function(data){
-<<<<<<< HEAD
   console.log("https://api.wmata.com/NextBusService.svc/json/jPredictions?StopID="+StopID+"&api_key="+api_key)
   console.log(data);
   BusTable(data);
-  //$('#BusWidget').append(
-    //$.map(data.Predictions, function(ignore, index){
-      //return 'Route ID: '+data.Predictions[index].RouteID+', Direction: '+data.Predictions[index].DirectionText+', time till arrival: '+data.Predictions[index].Minutes+'<br>';
-    //}).join()
-  //);
-=======
-  console.log(data)
->>>>>>> origin/master
+  $('#BusWidget').append(
+    $.map(data.Predictions, function(ignore, index){
+      return 'Route ID: '+data.Predictions[index].RouteID+', Direction: '+data.Predictions[index].DirectionText+', time till arrival: '+data.Predictions[index].Minutes+'<br>';
+    }).join()
+  );
 })
 .fail(function(data){
   alert("BusSchedule Failure!");
@@ -116,7 +100,7 @@ function WeatherCall(lat,lon){
   .done(function(WeatherData){
     console.log("Weather works");
     console.log(WeatherData.main.temp);
-    $('#WeatherWidget').append('<p>Current Temperature: <br>'+WeatherData.main.temp);
+    $('#WeatherWidget').html('<p>Current Temperature: <br>'+WeatherData.main.temp);
   })
   .fail(function(WeatherData){
     console.log("Weather failure");
@@ -124,7 +108,7 @@ function WeatherCall(lat,lon){
 }
 function BusTable(bus){
   var $table = $('<table/>');
-  $table.append("<th>Route ID</th><th>Direction</th><th>Minutes till arrival</th>");
+  $table.html("<th>Route ID</th><th>Direction</th><th>Minutes till arrival</th>");
   //$('#BusWidget').html(
     $.map(bus.Predictions, function(ignore,index){
       $table.append(
@@ -132,7 +116,7 @@ function BusTable(bus){
       '<td>'+bus.Predictions[index].DirectionText+'</td>'+
       '<td id="time">'+bus.Predictions[index].Minutes+'</td></tr>'
       );
-      $('#BusWidget').append($table);
+      $('#BusWidget').html($table);
     })
     AlertBus();
 }
